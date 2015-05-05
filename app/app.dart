67525d72f63f5ef08a172fd947a538adbf50f7cc
David Sergey <nirth.furzahad@gmail.com>
2015-05-05 14:10:01 +0200

    Updated app

--- app/app.dart
+++ app/app.dart
@@ -6,12 +6,13 @@ import 'package:image/image.dart';
 void main(List<String> arguments) {
   Grid bt = BinaryTree.start(new Grid(13, 13));
   Grid sw = Sidewinder.start(new Grid(13, 13));
+  Dijkstra dijkstra = new Dijkstra();
 
 //  Distances btd = new Distances(bt.cellAt(0, 0));
-  Distances d0x0 = Dijkstra.distances(sw.cellAt(0, 0));
+  Distances d0x0 = dijkstra.distances(sw.cellAt(0, 0));
   MaxDistance md = d0x0.maxDistance;
 
-  Distances dMax = Dijkstra.distances(md.cell);
+  Distances dMax = dijkstra.distances(md.cell);
   Distances path = dMax.pathTo(dMax.maxDistance.cell);
 
   Ascii ascii = new Ascii(distances: path);
