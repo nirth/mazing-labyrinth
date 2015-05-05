7d931f108e4d7fc00a8c357598ceb7d08756ea7d
David Sergey <nirth.furzahad@gmail.com>
2015-05-05 13:27:11 +0200

    Naive Dijkstra algorithm implementation

---
+++ lib/labyrinth/pathfinding/dijkstra.dart
@@ -0,0 +1,24 @@
+part of labyrinth;
+
+class Dijkstra {
+  static Distances distances(Cell cell) {
+    Distances result = new Distances(cell);
+    var frontier = [cell];
+
+    while(frontier.length > 0) {
+      var newFrontier = [];
+
+      frontier.forEach((Cell frontierCell) {
+        frontierCell.links.forEach((Cell linkedCell) {
+          if (result[linkedCell] == -1) {
+            result[linkedCell] = result[frontierCell] + 1;
+            newFrontier.add(linkedCell);
+          }
+        });
+      });
+      frontier = newFrontier;
+    }
+
+    return result;
+  }
+}
